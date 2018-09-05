5182f39a189bec9060b3f71acacbbaf216f49013
Romario Pontes <romario.pontes@dce.ufpb.br>
2018-09-04 21:08:39 -0300

    13 testes passando

--- src/main/java/br/ufpb/dcx/aps/carcassone/Jogo.java
+++ src/main/java/br/ufpb/dcx/aps/carcassone/Jogo.java
@@ -36,7 +36,7 @@ public class Jogo {
 		}
 		
 		if(isIniciada){
-			throw new ExcecaoJogo("Não pode iniciar uma partida enquanto a partida anterior não for finalizada");
+			throw new ExcecaoJogo("Nãoo pode iniciar uma partida enquanto a partida anterior não for finalizada");
 		}
 		isIniciada = true;
 		pegarProximoTile();
@@ -48,15 +48,13 @@ public class Jogo {
 	}
 
 	public String relatorioPartida() {
-		if (isIniciada == false) {
-			throw new ExcecaoJogo("Partida não iniciada");
-		}
-
+	
 		String sequencia = "";
 		
 		for (int i = 0; i < pecaDoJogador.length - 1; i++) {
 			sequencia += pecaDoJogador[i].toString() + ", ";
 		}
+		
 		
 		sequencia += pecaDoJogador[pecaDoJogador.length - 1];
 		if (status.equals("Fim")) {
@@ -106,14 +104,18 @@ public class Jogo {
 			++indiceDePecas;
 			proximaPecaDoJogador = pecaDoJogador[indiceDePecas % pecaDoJogador.length];
 		}
+		
 		return this;
 	}
 
 
 	public Jogo posicionarTile(Tile tileReferencia, Lado ladoTileReferencia) {
+		status = "Tile";
+		proximoTile = tilesUsar.get(tilesUsar.size() - 1);
+		tabuleiro.posicionar(tileReferencia, ladoTileReferencia, proximoTile);
+		pegarProximoTile();
 		return this;		
 	}
-	
 	public Jogo posicionarMeepleEstrada(Lado lado) {
 		return this;
 	}
